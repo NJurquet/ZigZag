b5c1e698b304d478f1ca3211d3c7e1ace4fcb195
NJurquet <nicolor01@hotmail.com>
2024-12-10 22:17:00 +0100

    Add `config.py`

---
+++ src/config.py
@@ -0,0 +1,24 @@
+"""
+config.py
+
+This module contains all configuration constants and parameter constants used throughout the project.
+It serves as a centralized location for managing settings and values that are used in multiple
+parts of the application.
+"""
+
+from enum import Enum
+
+# CONFIGURATION ###################
+WINDOW_NAME = "BlueStacks App Player"
+VISION_EN = True
+
+
+# PARAMETERS ######################
+
+
+class Colors(Enum):
+    """
+    Enum class for colors (in RGB) used in the project.
+    """
+    BALL_DRAWING_COLOR = (0, 0, 255)
+    EDGE_DRAWING_COLOR = (0, 255, 0)
