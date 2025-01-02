ad194471ddc42a1f23652057b585574381e8b65d
NJurquet <nicolor01@hotmail.com>
2025-01-02 14:00:10 +0100

    Document `DrawingManager`

--- src/ui/drawing_manager.py
+++ src/ui/drawing_manager.py
@@ -7,10 +7,34 @@ from ..config import Colors
 class DrawingManager:
     @staticmethod
     def draw_ball(img: np.ndarray, x: int, y: int, r: int) -> None:
+        """
+        Draws a circle on the image.
+
+        Parameters
+        ----------
+        `img` : `np.ndarray`
+            The image to draw the circle on.
+        `x` : `int`
+            The x-coordinate of the center of the circle.
+        `y` : `int`
+            The y-coordinate of the center of the circle.
+        `r` : `int`
+            The radius of the circle.
+        """
         cv2.circle(img, (x, y), r, Colors.BALL_DRAWING_COLOR.value, 3)
 
     @staticmethod
     def draw_path_edges(img: np.ndarray, lines: np.ndarray) -> None:
+        """
+        Draws path edges lines in the image.
+
+        Parameters
+        ----------
+        `img` : `np.ndarray`
+            The image to draw the path edges lines on.
+        `lines` : `np.ndarray`
+            The lines to draw, coming from HoughLinesP.
+        """
         if lines is not None:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
@@ -18,6 +42,21 @@ class DrawingManager:
 
     @staticmethod
     def get_path_edges_image(img: np.ndarray, lines: np.ndarray) -> np.ndarray:
+        """
+        Generates a black image with white path edges lines drawn on it.
+
+        Parameters
+        ----------
+        img : np.ndarray
+            Frame of the captured window.
+        lines : np.ndarray
+            Lines detected using HoughLinesP.
+
+        Returns
+        -------
+        np.ndarray
+            Binary image with black background and white path edges lines.
+        """
         lines_img = np.zeros_like(img)
         if lines is not None:
             for line in lines:
@@ -27,4 +66,14 @@ class DrawingManager:
 
     @staticmethod
     def draw_fps(img: np.ndarray, fps: float) -> None:
+        """
+        Draws the FPS on the frame.
+
+        Parameters
+        ----------
+        img : np.ndarray
+            The frame to draw the FPS on.
+        fps : float
+            The FPS value to draw.
+        """
         cv2.putText(img, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
